64e4f4df58fba10ec68162209a49ac7ab35decf0
Azamat <az1k3@outlook.com>
2021-09-04 14:37:28 +0500

    Backend / WebSocket done. Init

---
+++ src/components/JoinBlock.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import socket from '../socket';
+
+export default function JoinBlock() {
+  return (
+    <div className="join-block">
+      <input type="text" placeholder="Room ID" />
+      <input type="text" placeholder="Ваше имя" />
+      <button className="btn btn-success">Войти</button>
+    </div>
+  );
+}
